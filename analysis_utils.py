33ff2e235cf8b27d91e8fa13cd36a7a189591e1a
Kajanan Selvanesan <kajananselvanesan@gmail.com>
2024-07-01 01:05:13 +0530

    Debug code

--- analysis_utils.py
+++ analysis_utils.py
@@ -1233,6 +1233,7 @@ def purge_analysis_df(analysis_df, config, is_log_progress=True):
 
         # index row
         row = filter_dataframe_by_dict(analysis_df.dataframe, filter)
+        print(analysis_df.dataframe)
 
         # check and warning
         if (row is not None) and (len(row.index) > 0):
